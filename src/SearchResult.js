b97c062602468dfbb14ea0223b50ca3ac185abcc
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-09-19 14:24:12 +0700

    Created SearchResult.js

---
+++ src/SearchResult.js
@@ -0,0 +1,31 @@
+function SearchResult() {
+  return (
+    <div class="results">
+      <span>
+        <small>www.bing.com</small>
+      </span>
+      <h2>
+        <a href="https://www.bing.com" target="_blank">
+          JavaScript tutorial - w3Shool
+        </a>
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem,
+        deleniti delectus possimus sapiente doloribus laboriosam alias dolor.
+        Officiis, magni! Temporibus deserunt molestias nobis modi eius
+        consequatur. Aut quae obcaecati repellat.
+      </p>
+      <ul>
+        <li>
+          <a href="haikuacademy.com">basdbbr intro</a>
+        </li>
+        <li>
+          <a href="jdbhgkjbgfew.net"> kjgherjgb school</a>
+        </li>
+        <li>
+          <a href="fbwgeouf.org">kjwbubeq video</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
