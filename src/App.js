9fc201427df80b06dab96f67869c8651aa5efcfe
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-09-19 14:23:35 +0700

    Updated App.js with data array containing objects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
 import logo from './logo.svg'
 import './App.css'
 
+let data = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+]
+
 function App() {
   return (
     <div class="wrapper">
