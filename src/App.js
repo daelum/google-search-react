0481fc4edb311b02a49ddae5c18be3aa2df493d0
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-09-20 15:24:35 +0700

    imported SearchResult/ Added SearchResult Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
+import SearchResult from './SearchResult'
 
 function App() {
   let data = [
@@ -49,9 +50,24 @@ function App() {
         },
       ],
     },
+    {
+      title: 'JS tutorials',
+      description: 'The best JavaScript tutorials in the galaxy!',
+      url: 'https://www.w3schools.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.w3schools.com/js',
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.w3schools.com/js',
+        },
+      ],
+    },
   ]
 
-  const [name, setName] = useState('')
+  // const [name, setName] = useState('')
 
   return (
     <div class="wrapper">
@@ -72,28 +88,7 @@ function App() {
             <small>{data.length} Results</small>
           </span>
         </div>
-        <div class="results">
-          <span>
-            <small>{data[0].url}</small>
-          </span>
-          <h2>
-            <a href={data[0].url} target="_blank">
-              {data[0].title}
-            </a>
-          </h2>
-          <p>{data[0].description}</p>
-          <ul>
-            <li>
-              <a href={data[0].links[0].url}>{data[0].links[0].title}</a>
-            </li>
-            <li>
-              <a href={data[0].links[1].url}>{data[0].links[1].title}</a>
-            </li>
-            <li>
-              <a href="fbwgeouf.org">kjwbubeq video</a>
-            </li>
-          </ul>
-        </div>
+        <SearchResult data={data} />
         <div class="results">
           <span>
             <small>{data[1].url}</small>
