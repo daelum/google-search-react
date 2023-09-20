58e2c95269e4a3524ae21242d3920097d23b2cd2
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-09-20 21:06:37 +0700

    fixed claaNames

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchResult from './SearchResult'
 function App() {
   let data = [
     {
-      title: 'JS tutorials',
+      title: 'JS tutorisdals',
       description: 'The best JavaScript tutorials in the galaxy!',
       url: 'https://www.w3schools.com',
       links: [
@@ -51,7 +51,7 @@ function App() {
       ],
     },
     {
-      title: 'JS tutorials',
+      title: 'JS tutoriaaaals',
       description: 'The best JavaScript tutorials in the galaxy!',
       url: 'https://www.w3schools.com',
       links: [
@@ -80,65 +80,25 @@ function App() {
   // }
 
   return (
-    <div class="wrapper">
-      <div class="header">
-        <div class="logo">
+    <div className="wrapper">
+      <div className="header">
+        <div className="logo">
           <img src="images/google.png" alt="Google Logo" />
         </div>
         <form>
           {/* <!-- We need both tags next to each other to avoid the extra space --> */}
-          <input type="text" class="input-bar" id="inputText" />
-          <button class="search-button">Search</button>
+          <input type="text" className="input-bar" id="inputText" />
+          <button className="search-button">Search</button>
         </form>
       </div>
-      <div class="all-results">
-        <div class="number-of-results">
+      <div className="all-results">
+        <div className="number-of-results">
           <span>
             {/* Replace the hardcoded number of results with the length of the array. */}
             <small>{data.length} Results</small>
           </span>
         </div>
         <SearchResult data={data} />
-        <div class="results">
-          <span>
-            <small>{data[1].url}</small>
-          </span>
-          <h2>
-            <a href={data[1].url}>{data[1].title}</a>
-          </h2>
-          <p>{data[1].description}</p>
-          <ul>
-            <li>
-              <a href={data[1].links[0].url}>{data[1].links[0].title}</a>
-            </li>
-            <li>
-              <a href={data[1].links[1].url}>{data[1].links[1].title}</a>
-            </li>
-            <li>
-              <a href="fbwgeouf.org">kjwbubeq video</a>
-            </li>
-          </ul>
-        </div>
-        <div class="results">
-          <span>
-            <small>{data[2].url}</small>
-          </span>
-          <h2>
-            <a href={data[2].url}>{data[2].title}</a>
-          </h2>
-          <p>{data[2].description}</p>
-          <ul>
-            <li>
-              <a href={data[2].links[0].url}>{data[2].links[0].title}</a>
-            </li>
-            <li>
-              <a href={data[2].links[1].url}>{data[2].links[1].title}</a>
-            </li>
-            <li>
-              <a href="fbwgeouf.org">kjwbubeq viadgho</a>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   )
